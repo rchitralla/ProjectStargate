631a4a894bda13ffe0dfd81d9611eb01a94e8990
Regina (Reg) Chitralla <reginachitralla@gmail.com>
2024-06-30 00:59:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,5 +121,17 @@ def main():
 
     st.write("Created by Regina Chitralla")
 
+    # Add PayPal donation button
+    st.markdown(
+        """
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <input type="hidden" name="hosted_button_id" value="8TTFV24WNWLS4" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+        """,
+        unsafe_allow_html=True
+    )
+
 if __name__ == "__main__":
     main()
